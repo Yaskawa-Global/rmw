64df447e7fe0f340099f847296e5efc45e41cf3c
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2021-09-24 16:59:12 +0200

    No support for deprecation msgs

--- rmw/include/rmw/types.h
+++ rmw/include/rmw/types.h
@@ -409,9 +409,14 @@ enum RMW_PUBLIC_TYPE rmw_qos_liveliness_policy_t
   /// Explicitly asserting node liveliness is required in this case.
   /// This option is deprecated, use RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_TOPIC if your application
   /// requires to assert liveliness manually.
+#ifndef OLD_COMPILER_NO_SUPPORT
   RMW_DECLARE_DEPRECATED(
     RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_NODE,
     RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_NODE_DEPRECATED_MSG) = 2,
+#else
+  #warning Removed ATTRIBUTE DEPRECATED here
+    RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_NODE = 2,
+#endif
 
   /// The signal that establishes a Topic is alive is at the Topic level. Only publishing a message
   /// on the Topic or an explicit signal from the application to assert liveliness on the Topic
